3ad1025c8e13c2e3a09a8c8f1bcdb320066f6318
Mehedi Hasan Kajol <mehedi.kajol.ice@gmail.com>
2022-10-03 19:31:55 +0600

    added intitial migration

--- ECom.Application/Products/CreateProduct.cs
+++ ECom.Application/Products/CreateProduct.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using ECom.Database;
+using ECom.Domain.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,23 @@ using System.Threading.Tasks;
 
 namespace ECom.Application.Products
 {
-    internal class CreateProduct
+    public class CreateProduct
     {
+        private readonly AppDbContext _context;
+        public CreateProduct(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public void Do(int ProductId, string Name, string Description, decimal Price)
+        {
+            _context.Products.Add(new Product
+            {
+                ProductId = ProductId,
+                Name = Name,
+                Description = Description,
+                Price = Price
+            });
+        }
     }
 }
---
+++ ECom.Database/AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using ECom.Domain.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECom.Database
+{
+    public class AppDbContext : IdentityDbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options) { }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
